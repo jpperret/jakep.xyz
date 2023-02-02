8055e5164a9c419d0a5d6e7570e7eb597d77432a
Jake Perret <djakeperret@gmail.com>
2023-02-01 20:01:59 -0500

    update bracket builder text

--- src/pages/BracketBuilder.js
+++ src/pages/BracketBuilder.js
@@ -18,24 +18,26 @@ export default function BracketBuilder() {
       </Heading>
       <Text>
         Bracket Builder is an iOS app that allows users to create and track
-        custom tournaments.
+        custom tournaments
       </Text>
-      <Text>Easily track and share tournament status.</Text>
+      <Text>Easily track and share tournament status</Text>
+      <Text>Requires iOS 16.0 or newer</Text>
       <Heading as="h2" size="lg">
         Features
       </Heading>
+      <Text>All features are free to use</Text>
       <UnorderedList>
         <ListItem>
-          Create single or double elimination brackets of sizes ranging from 2
-          teams to 32 teams
+          Supports creating brackets of sizes ranging from 2 teams to 16 teams
         </ListItem>
+        <ListItem>Single and double elimination brackets</ListItem>
         <ListItem>Automatic ordering of teams based on seed</ListItem>
-        <ListItem>Double elimination bracket support</ListItem>
-        <ListItem>Track winners and losers</ListItem>
+        <ListItem>Optionally randomize team seeds</ListItem>
+        <ListItem>Keeps track of winners and losers</ListItem>
         <ListItem>Easily remove winners</ListItem>
-        <ListItem>All features are free to use</ListItem>
+        <ListItem>Light and dark mode support</ListItem>
         <ListItem>
-          Share bracket as an image with participants or friends
+          Share brackets as an image with participants or friends
         </ListItem>
       </UnorderedList>
       <br /> {/* TODO: Should be just bottom padding */}
@@ -43,6 +45,16 @@ export default function BracketBuilder() {
         Organize and track tournaments for pool, tennis, baseball, basketball,
         pong, die and anything else you can think of!
       </Text>
+      <Heading as="h2" size="lg">
+        Feedback
+      </Heading>
+      <Text>
+        Please send any bug reports or other suggestions to{" "}
+        <Link href="mailto:brackbuilder@gmail.com">brackbuilder@gmail.com</Link>
+      </Text>
+      <Heading as="h2" size="lg">
+        Privacy Policy
+      </Heading>
       <Text>
         <Link href="/bracket-builder/privacy">
           Bracket Builder Privacy Policy
